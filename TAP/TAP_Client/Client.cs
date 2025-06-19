5b6e3e2b55294a837b3c83d2d56cdc5926c7df89
YongminGwon <clumsyrhymer@gmail.com>
2025-06-19 10:34:10 +0900

    TAP Done

---
+++ TAP/TAP_Client/Client.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TAP_Client;
+internal class Client
+{
+    static async Task Main(string[] args)
+    {
+        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        EndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+        await socket.ConnectAsync(endPoint);
+        while (true)
+        {
+            string str = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(str);
+            await socket.SendAsync(buffer, SocketFlags.None);
+        }
+    }
+}
---
+++ TAP/TAP_Server/Server.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TAP_Server;
+internal class Server
+{
+    static async Task Main(string[] args)
+    {
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        EndPoint endPoint = new IPEndPoint(IPAddress.Any, 12345);
+        serverSocket.Bind(endPoint);
+        serverSocket.Listen(1000);
+
+        while (true)
+        {
+            Socket clientSocket = await serverSocket.AcceptAsync();
+            Console.WriteLine($"Client connected: {clientSocket.RemoteEndPoint}");
+            _ = HandleClientAsync(clientSocket);
+        }
+    }
+    static async Task HandleClientAsync(Socket clientSocket)
+    {
+        var buffer = new byte[256];
+        while(true)
+        {
+            int received = await clientSocket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+            if(received == 0)
+            {
+                break;
+            }
+            string receivedData = Encoding.UTF8.GetString(buffer, 0, received);
+            Console.WriteLine($"Received from {clientSocket.RemoteEndPoint}: {receivedData}");
+        }
+        Console.WriteLine($"Client disconnected: {clientSocket.RemoteEndPoint}");
+        clientSocket.Close();
+    }
+}
